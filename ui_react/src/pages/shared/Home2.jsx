29353eab80fd1058d5cbee0c5e85ac92ecd9b75c
gijendrap <144902090+gijendrap@users.noreply.github.com>
2024-03-16 18:07:45 +0530

    second

--- ui_react/src/pages/shared/Home2.jsx
+++ ui_react/src/pages/shared/Home2.jsx
@@ -14,11 +14,6 @@ const App = () => {
                 </div>
             </div>
             {/* Container */}
-                        {/* Container */}
-            {/* Container */}
-            {/* Container */}
-            {/* Container */}
-
             <div className="container px-4 md:px-0 max-w-6xl mx-auto -mt-32">
                 <div className="mx-0 sm:mx-6">
                     {/* Nav */}
